f4a2a7571d7620e877170e05429828a8b84031db
deathgrindfreak <john.cooper.bell@gmail.com>
2016-04-25 19:19:15 +0000

    Finish 1-10

--- 1-10/Problems.hs
+++ 1-10/Problems.hs
@@ -1,4 +1,4 @@
-module Problems1to10 where
+module Problems where
 
 -- Problem 1
 myLast :: [a] -> a
@@ -34,4 +34,24 @@ isPalindrome xs = and $ map (\(x, y) -> x == y) $ zip xs (reverse xs)
 -- Problem 7
 data NestedList a = Elem a | List [NestedList a]
 flatten :: NestedList a -> [a]
-flatten xs = undefined
+flatten (Elem a) = [a]
+flatten (List []) = []
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+          
+-- Problem 8
+compress :: Eq a => [a] -> [a]
+compress [] = []
+compress (x:xs) = x : compress (dropWhile (==x) xs)
+
+-- Problem 9
+pack :: Eq a => [a] -> [[a]]
+pack [] = []
+pack (x:xs) = p [x] xs
+    where p _ [] = []
+          p (x:xs) (y:ys)
+            | x == y    = p (y:x:xs) ys
+            | otherwise = (x:xs) : p [y] ys
+            
+-- Problem 10
+encode :: Eq a => [a] -> [(Int, a)]
+encode = map (\l@(x:_) -> (length l, x)) . pack
---
+++ 11-20/Problems.hs
@@ -0,0 +1,3 @@
+module Problems where
+
+-- Problem 11
